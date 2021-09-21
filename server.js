0eb96f1a18087fff53d1cf9598ff04612e7c781d
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-09-21 16:38:33 +0300

    done

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
+require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
 const axios = require('axios')
 
 const server = express();
